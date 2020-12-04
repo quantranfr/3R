54e0f4777d849138555b9f92310b7df6edb8e67c
quantranfr <quan.tran.fr@gmail.com>
2020-12-04 23:30:28 +0700

    Runnable on Raspberry Pi 4b

---
+++ docs/README.md
@@ -0,0 +1,44 @@
+# Method 3R for environment protection
+
+## Motivation
+
+Today more and more Vietnamese are aware of environmental issues, amongst them daily waste concerns everyone. Slogan and propaganda are many, but concret actions are too neglected. This project is born to fill that gap between green spirit and green actions.
+
+## How it works
+
+3R is a famous mnemotechnic to remind how to deal with a certain type of product. 3R represents *recycle, reduce, reuse*. In Vietnamese, they are translated into 3T: *tái chế, tiết giảm, tái sử dụng*.
+
+The player will choose one of the 3 Rs, and one of the given products. The machine will suggest a way to protect the environment based on the player's choices.
+
+Play the video below to grasp what all of this is about:
+    
+[![](http://img.youtube.com/vi/sBtvLVopoT8/0.jpg)](https://youtu.be/sBtvLVopoT8)
+
+## Pre-requisites
+
+Materials:
+    
+* 2 NFC reader modules as in [our multiple-objects detection system](https://github.com/quantranfr/MultiNFC);
+* 3 daisy-chained LED matrix modules (32x64 full color) and a Raspberry Pi (tested with a 4b) as described in [my LED matrix investigation](https://github.com/quantranfr/LEDMatrix).
+
+Libraries and code setup:
+    
+* Download [MultiNFC](https://github.com/quantranfr/MultiNFC)
+* Download [rpi-rgb-led-matrix](https://github.com/hzeller/rpi-rgb-led-matrix/)
+
+  * Go to the subfolder `examples-api-use` and run `make`; this will generate the `demo` executable that we will use in our example below;
+  * Run `sudo setcap 'cap_sys_nice=eip' demo` to be able to run `demo` correctly (no flickering) without `sudo`.
+  * Open `/boot/config.txt` in sudo mode and change the line `dtparam=audio=on` to `dtparam=audio=off`;
+
+* Install `pango` with `sudo apt-get install -y libsdl-pango-dev`. In MacOS: `brew install pango`.
+
+## Run the program
+
+Follow these steps:
+    
+* Run `python main.py` in this repository.
+* Run `python readSerial.py` in MultiNFC.
+
+## Roadmap
+
+* Make a second LED matrix line to display user's choices.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import websockets
 import os
+import subprocess
+from time import time
 
 methods = {
   "79 AB 75 B2": "Tái chế",
@@ -26,19 +28,45 @@ solutions = { # from the point of view of an ordinary citizen
   ("Tiết giảm", "Pin"): "Hạn chế tặng đồ chơi dùng pin cho trẻ em!"
 }
 
+last_solution = None # last message shown on the display
+last_time = 0        # last timestamp of a valid operation
+p = None             # a Popen object
+displayer = input("Path to the demo program of rpi-rgb-led-matrix library [/home/pi/Soft/rpi-rgb-led-matrix/examples-api-use/demo]:")
+if displayer == "":
+    displayer = "/home/pi/Soft/rpi-rgb-led-matrix/examples-api-use/demo"
+
 async def hello(websocket, path):
+    global last_solution, p, last_time, displayer
+    
     mes = await websocket.recv()
     readers = mes.split(';')
     if len(readers) == 2:
         cards = [reader.split(':')[1] for reader in readers]
         if cards[0] in methods:
-            method, object = methods[cards[0]], objects[cards[1]]
+            method, obj = methods[cards[0]], objects[cards[1]]
         else:
-            method, object = methods[cards[1]], objects[cards[0]]
+            method, obj = methods[cards[1]], objects[cards[0]]
 
-        solution = solutions[(method, object)]
-        os.system(f'pango-view --font=Courier --antialias=none -o image.png -t "{solution}" --margin=4,50,3,50 --dpi=150') # will produce a ..x32 image
+        solution = solutions[(method, obj)]
+        
+        if solution == last_solution:
+            if time() - last_time > 60:
+                p.terminate()
+            return
+        else:
+            if p is not None:
+                p.terminate()
+                
+        last_solution = solution
+        last_time = time()
+        
+        command = f'pango-view --font=Courier --background=black --foreground=white -qo image.png -t "{solution}" --margin=4,50,3,50 --dpi=150' # will produce a ..x32 image
+        os.system(command)
+                
         print(f"< {solution}")
+        os.system('convert image.png image.ppm')
+        command = f'{displayer} --led-rows=32 --led-cols=64 --led-slowdown-gpio=4 --led-chain=3 -D1 image.ppm --led-no-hardware-pulse'
+        p = subprocess.Popen(command.split(' '))
 
 start_server = websockets.serve(hello, "localhost", 9080)
 
